b4e7f62dc6ddda46aecaea5fc747906177277932
Sasologic <sasologic@gmail.com>
2020-10-31 07:09:11 +0100

    Plethora of adjustment involving index.html, main.css and index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@ const modal = document.querySelector("#modal");
 console.log(modal);
 const modalButton = document.querySelector("#modal-button");
 
-//setTimeout(modal.classList.remove("hide-modal"),5000);
+setTimeout( () => {
+  modal.classList.remove("hide-modal");
+  modal.classList.add("modal");
+},3000);
 
 modalButton.addEventListener('click', () => {
   modal.classList.add("hide-modal");
