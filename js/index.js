6e07ec037f8d5e12d76ed825abe30c9a433c4f2b
Sasologic <sasologic@gmail.com>
2020-11-16 13:22:59 +0100

    Modified index.js, index.html and main.css

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Code for Modal
 const modal = document.querySelector("#modal");
 console.log(modal);
 const modalButton = document.querySelector("#modal-button");
@@ -11,4 +12,18 @@ modalButton.addEventListener('click', () => {
   modal.classList.add("hide-modal");
 });
 
+//Code for Toggling mobile views
+const menu = document.getElementById("mobile-icon");
+const nav = document.getElementById("nav");
+const exit = document.getElementById("exit");
+
+menu.addEventListener('click', function(e) {
+  nav.classList.toggle('hide-mobile');
+  e.preventDefault();
+});
+
+exit.addEventListener('click', function(e) {
+  nav.classList.add('hide-mobile');
+  e.preventDefault();
+});
 
